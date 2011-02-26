53c335296b6ca3623377fae027df538447912e3f
yaroslavnedash <yaroslav12345@ubuntu.(none)>
2011-02-26 16:29:14 +0200

    ticket:3 for #4, release :)

--- test_yarned_app/views.py
+++ test_yarned_app/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render_to_response
+from django.template import RequestContext
 
 from test_yarned_app.models import Person
 from test_yarned_app.models import RequestSnapShot
@@ -6,7 +7,8 @@ from test_yarned_app.models import RequestSnapShot
 
 def person_info(request):
     person = Person.objects.get(id=1)
-    return render_to_response('main.html', {'person': person})
+    return render_to_response('main.html',
+        context_instance=RequestContext(request, {'person': person}))
 
 
 def requests_info(request):
